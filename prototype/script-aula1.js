37b0b570eaa63c5d3e5f7b5e3b27aeabc8a81156
Pegasis12 <plsantana246@gmail.com>
2025-07-14 16:54:39 -0300

    ajustes

--- prototype/script-aula1.js
+++ prototype/script-aula1.js
@@ -10,23 +10,26 @@ function Pessoa(nome, sobrenome, idade){
 Pessoa.prototype.nomeCompleto = function(){
   return `${this.nome} ${this.sobrenome}`;
 }
-
-const joao = new Pessoa("Pablo", "Queiroz", 23); 
+const joao = new Pessoa("Pablo", "Queiroz", 23); //criando uma pessoa pablo com o construtor Pessoa
 
 // Liste os métodos acessados por 
 // dados criados com NodeList,
 // HTMLCollection, Document
+Object.getOwnPropertyNames(NodeList.prototype); //getOwnPropertyNames // Retorna uma lista com os métodos / propriedades do seu contrutor
+Object.getOwnPropertyNames(HTMLCollection.prototype);
+Object.getOwnPropertyNames(Document.prototype);
+
 
 // Liste os construtores dos dados abaixo
 const li = document.querySelector('li');
 
-li;
-li.click;
-li.innerText;
-li.value;
-li.hidden;
-li.offsetLeft;
-li.click();
+li; //li.constructor.name; 'HTMLLIElement'
+li.click; //li.click.constructor.name; 'Function'
+li.innerText; //li.innerText.constructor.name; 'String'
+li.value; //li.value.constructor.name; 'Number'
+li.hidden; //li.hidden.constructor.name; 'Boolean'
+li.offsetLeft; //li.offsetLeft.constructor.name; 'Number'
+li.click(); //não existe
 
 // Qual o construtor do dado abaixo:
-li.hidden.constructor.name;
+li.hidden.constructor.name; //'Boolean'
